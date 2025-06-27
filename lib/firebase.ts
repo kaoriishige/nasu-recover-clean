22bf884e6edfd483777ddf06e6e1979bcf1a96dc
kaoriishige <adtown001@gmail.com>
2025-06-27 13:07:00 +0900

    fix: export app for AdminLayout

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,22 +1,23 @@
 // lib/firebase.ts
-import { initializeApp, getApps, getApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyDtTt0fWthsU6Baq1fJwUx8CgSakoZnMXY",
+  authDomain: "minna-no-nasu-app.firebaseapp.com",
+  projectId: "minna-no-nasu-app",
+  storageBucket: "minna-no-nasu-app.appspot.com",
+  messagingSenderId: "885979930856",
+  appId: "1:885979930856:web:2b06441bc2d497cf3e695d",
+  measurementId: "G-FNY4RBQKHW",
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-
-
+export { app };
 
 
 
